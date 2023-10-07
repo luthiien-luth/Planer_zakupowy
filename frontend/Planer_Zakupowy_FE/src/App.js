d957ddad9482c329e785cc7e0a1cfc85bad24302
Kasia <kasia1781@gmail.com>
2023-10-07 17:19:05 +0200

    Added Layout Component

--- frontend/Planer_Zakupowy_FE/src/App.js
+++ frontend/Planer_Zakupowy_FE/src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import Header from './components/Header';
 import './App.css';
+import Layout from './components/Layout/Layout';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    <Header title="Planer zakupowy" />
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+    return <Layout />;
 }
 
 export default App;
