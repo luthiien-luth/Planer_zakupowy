4a659d1e940af6c2dbd0af6ae38d69cb830acd52
Edyta <ulanowicz.edyta@gmail.com>
2023-11-26 15:25:21 +0100

    refactor: Enhance and wrap TextInput component with Box

--- frontend/Planer_Zakupowy_FE/src/components/TextInput/TextInput.jsx
+++ frontend/Planer_Zakupowy_FE/src/components/TextInput/TextInput.jsx
@@ -1,22 +1,32 @@
 import { Controller } from 'react-hook-form';
 
 //components MUI
-import TextField from '@mui/material/TextField';
+import { TextField, Box } from '@mui/material';
 
-export const TextInput = ({ formControl, label, rules }) => {
+export const TextInput = ({ control, name, defaultValue, rules, id, label, type }) => {
     return (
-        <Controller
-            control={formControl}
-            name={name}
-            rules={rules}
-            render={() => (
-                <TextField id="outlined-basic" label={label} variant="outlined" style={styles} />
-            )}
-        />
+        <Box
+            sx={{
+                width: '340px',
+                margin: '16px'
+            }}>
+            <Controller
+                control={control}
+                name={name}
+                defaultValue={defaultValue}
+                rules={rules}
+                render={({ field: { onChange } }) => (
+                    <>
+                        <TextField
+                            variant="outlined"
+                            onChange={onChange}
+                            id={id}
+                            label={label}
+                            type={type}
+                        />
+                    </>
+                )}
+            />
+        </Box>
     );
-};
-
-const styles = {
-    width: 340,
-    margin: 10
 };
